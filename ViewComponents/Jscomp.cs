b80c5a5262873f0b8bdeb49fbd4d7f5e2d6a58ef
sergey.lvov <sergey.lvov70@gmail.com>
2021-04-06 09:52:01 +0300

    init

---
+++ ViewComponents/Jscomp.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspcompjs.ViewComponents
+{
+	public class Jscomp :ViewComponent
+	{
+		public Jscomp()
+		{
+			
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View();
+		}
+	}
+}
