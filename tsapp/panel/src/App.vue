1d5562c1fcff1f41d867a0af9fd6cd5ed72bfaa7
sergey.lvov <sergey.lvov70@gmail.com>
2021-04-06 12:10:00 +0300

    'vue'

---
+++ tsapp/panel/src/App.vue
@@ -0,0 +1,52 @@
+import Vue from 'vue';
+<template>
+<v-stage ref="stage" :config="konva">
+    <v-layer>
+      <v-line :config="line"></v-line>
+      <!--<v-circle :config="circle"></v-circle>-->
+    </v-layer>
+  </v-stage>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: function()
+  {
+    return {
+      name: 'Sample',
+      konva: {
+        width: 200,
+        height: 200
+      },
+      circle:{
+        x: 100,
+        y:100,
+        radius: 70,
+        fill: "black"
+      },
+      line:{
+      x: 100,
+  y: 50,
+  points: [0, 0, 200, 200],
+  stroke: 'red',
+  tension: 1
+      }
+
+    }
+
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ tsapp/panel/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import VueKonva from 'vue-konva'
+Vue.use(VueKonva)
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
